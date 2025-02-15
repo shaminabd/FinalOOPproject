c23d454268241f8b706da9c1d111393cf3e21a57
Ali-Baimen <240021@astanait.edu.kz>
2025-02-15 09:57:58 +0600

    check

--- src/Models/Admin.java
+++ src/Models/Admin.java
@@ -5,6 +5,7 @@ import Enums.UserRole;
 public class Admin extends User {
     public Admin(int id, String name, String email, String password) {
         super(id, name, email, password, UserRole.ADMIN);
+
     }
 
     public void manageSystem() {
